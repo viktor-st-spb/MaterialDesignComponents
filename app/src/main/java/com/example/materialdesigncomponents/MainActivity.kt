c6365c66b453b3cd4dcdd7c5e43ab8f10a8a6e05
Viktor Stiblo <viktor.st.spb@gmail.com>
2021-04-04 20:44:30 +0300

    First commit

---
+++ app/src/main/java/com/example/materialdesigncomponents/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.materialdesigncomponents
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn_snackBar.setOnClickListener {
+            Toast.makeText(
+                this, "SnackBar button pressed",
+                Toast.LENGTH_LONG
+            ).show()
+
+            val snackBarIntent =
+                Intent(this, SnackBarActivity::class.java)
+            startActivity(snackBarIntent)
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/materialdesigncomponents/SnackBarActivity.kt
@@ -0,0 +1,83 @@
+package com.example.materialdesigncomponents
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.transition.Fade
+import com.google.android.material.snackbar.BaseTransientBottomBar
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.transition.MaterialContainerTransform
+import kotlinx.android.synthetic.main.activity_snackbar.*
+
+class SnackBarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_snackbar)
+
+        btn_woActionShort.setOnClickListener {
+            Snackbar.make(constraintLayout,
+                "SnackBar w/o action SHORT", Snackbar.LENGTH_SHORT)
+                .show()
+        }
+
+        btn_woActionLong.setOnClickListener {
+            Snackbar.make(constraintLayout,
+                "SnackBar w/o action LONG", Snackbar.LENGTH_LONG)
+                .show()
+        }
+
+        btn_withAction_10s.setOnClickListener {
+            Snackbar.make(constraintLayout,
+                "SnackBar with action 10 s", Snackbar.LENGTH_LONG)
+                .setDuration(10000)
+                .setAction("BACK"){
+
+                    Toast.makeText(
+                        this,
+                        "Button BACK pressed",
+                        Toast.LENGTH_LONG).show()
+
+                }
+                .show()
+        }
+
+        btn_indefinite.setOnClickListener {
+            Snackbar.make(constraintLayout,
+                "SnackBar with action INDEFINITE",
+                Snackbar.LENGTH_INDEFINITE)
+//                .setDuration(10000)
+                .setAction("BACK"){
+
+                    Toast.makeText(
+                        this,
+                        "Button BACK pressed",
+                        Toast.LENGTH_LONG).show()
+
+                }
+                .show()
+        }
+
+        btn_anchor.setOnClickListener {
+            Snackbar.make(constraintLayout,
+                "SnackBar above FAB",
+                Snackbar.LENGTH_INDEFINITE)
+                .setAnchorView(floatingActionButton)
+//                .setAnimationMode(BaseTransientBottomBar.ANIMATION_MODE_SLIDE)
+                .setAnimationMode(BaseTransientBottomBar.ANIMATION_MODE_FADE)
+                .setAction("BACK"){
+
+                    Toast.makeText(
+                        this,
+                        "Button BACK pressed",
+                        Toast.LENGTH_LONG).show()
+
+                }
+                .show()
+        }
+
+
+
+    }
+}
